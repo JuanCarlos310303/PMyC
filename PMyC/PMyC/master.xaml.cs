adc8063559c2cd3e11a6bd8b5cafaa2523bc9398
JuanCarlos310303 <carloz310303@gmail.com>
2021-04-25 17:12:33 -0500

    gg

---
+++ PMyC/PMyC/master.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PMyC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class master : ContentPage
+    {
+        public master()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnmru_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new mru());
+        }
+
+        private async void btnmrua_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new mrua());
+        }
+
+        private  async void btnsln_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new sln());
+        }
+
+        private async void btntln_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new tln());
+        }
+
+        private async void btnd_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new datos());
+        }
+
+        private async void btnlgu_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new lgu());
+        }
+    }
+}
