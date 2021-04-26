1c73e2d68824e17c40948be744cc25a16a068041
GussX03 <62187656+GussX03@users.noreply.github.com>
2021-04-26 09:46:11 -0500

    Update lgu.xaml.cs

--- PMyC/PMyC/lgu.xaml.cs
+++ PMyC/PMyC/lgu.xaml.cs
@@ -10,11 +10,29 @@ using Xamarin.Forms.Xaml;
 namespace PMyC
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class lgu : ContentPage
+    public partial class Igu : ContentPage
     {
-        public lgu()
+        public Igu()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtN1.Text) && !String.IsNullOrEmpty(txtN2.Text ) && !String.IsNullOrEmpty(txtN3.Text) && !String.IsNullOrEmpty(txtN4.Text))
+            {
+                var g = double.Parse(txtN1.Text);
+                var m1 = double.Parse(txtN2.Text);
+                var m2 = double.Parse(txtN3.Text);
+                var d2 = double.Parse(txtN4.Text);
+                var fuerza = (g*((m1*m2)/(d2*d2))) +" N";
+                resultado.Text = fuerza.ToString();
+            }
+            else
+            {
+                DisplayAlert("Datos incompletos", "Escribe todos los datos", "ok");
+            }
+
         }
     }
 }
