1f858cd8ee6673bb2318d906c634c1da8f3ed052
GussX03 <62187656+GussX03@users.noreply.github.com>
2021-04-26 09:38:21 -0500

    Update mrua.xaml.cs

--- PMyC/PMyC/mrua.xaml.cs
+++ PMyC/PMyC/mrua.xaml.cs
@@ -16,5 +16,23 @@ namespace PMyC
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtN1.Text) && !String.IsNullOrEmpty(txtN2.Text ) && !String.IsNullOrEmpty(txtN3.Text) && !String.IsNullOrEmpty(txtN4.Text))
+            {
+                var g = double.Parse(txtN1.Text);
+                var m1 = double.Parse(txtN2.Text);
+                var m2 = double.Parse(txtN3.Text);
+                var d2 = double.Parse(txtN4.Text);
+                var fuerza = (g*((m1*m2)/(d2*d2))) +" N";
+                resultado.Text = fuerza.ToString();
+            }
+            else
+            {
+                DisplayAlert("Datos incompletos", "Escribe todos los datos", "ok");
+            }
+
+        }
     }
 }
